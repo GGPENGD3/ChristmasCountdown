421ae833c09e8dfa069f1e5ac1224d31f85625ff
Kaizatan <kaishenkaiser.t@digipen.edu>
2024-03-07 04:32:18 +0800

    Setup Main Menu + FPS Controller
    1.  Made a simple main menu
         (start/option/credit/exit)
    
    2. Added in the FPS controller script (currently attached to player 1, might need another if using controller for player 2)

---
+++ Assets/Scripts/Main Menu/MainMenuBrain.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MainMenuBrain : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    public void OptionsMenu()
+    {
+        //open up option menu
+    }
+
+    public void CreditsMenu()
+    {
+        //open up credits menu
+    }
+}
