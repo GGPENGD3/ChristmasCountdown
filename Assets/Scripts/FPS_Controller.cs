15e212975e4494a56f4d20631c817673c916f605
Kaizatan <kaishenkaiser.t@digipen.edu>
2024-03-18 17:49:52 +0800

    Edited Script To Hold 4 Controllers
    - Edited script to adjust the variable to whichever joystick based on the player attached to it except that P2, P3, P4 does not have they joysticks set up

--- Assets/Scripts/FPS_Controller.cs
+++ Assets/Scripts/FPS_Controller.cs
@@ -8,6 +8,14 @@ public class FPS_Controller : MonoBehaviour
     private Rigidbody rb;
     Vector3 targetVelocity;
 
+    public GameObject player;
+    string player_RightThumbStickHorizontal;
+    string player_RightThumbStickVertical;
+    string player_LeftThumbStickHorizontal;
+    string player_LeftThumbStickVertical;
+    string player_RightThumbstickBttn;
+    string player_B_Bttn;
+
     #region Camera Movement Variables
 
     public Camera playerCamera;
@@ -134,23 +142,63 @@ public class FPS_Controller : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        #region Camera
+        #region Find Player Number
+        //assigns a string to variable num based on the player assigned to this script
+        //num will be then used to determine which joystick is being used for input
+        if (player.name == "Player 1")
+        {
+            player_RightThumbStickHorizontal = "P1 Right ThumbStick Horizontal";
+            player_RightThumbStickVertical = "P1 Right ThumbStick Vertical";
+            player_LeftThumbStickHorizontal = "P1 Left ThumbStick Vertical";
+            player_LeftThumbStickVertical = "P1 Left ThumbStick Horizontal";
+            player_RightThumbstickBttn = "P1 RB";
+            player_B_Bttn = "P1 B";
+        }
+        else if (player.name == "Player 2")
+        {
+            player_RightThumbStickHorizontal = "P2 Right ThumbStick Horizontal";
+            player_RightThumbStickVertical = "P2 Right ThumbStick Vertical";
+            player_LeftThumbStickHorizontal = "P2 Left ThumbStick Vertical";
+            player_LeftThumbStickVertical = "P2 Left ThumbStick Horizontal";
+            player_RightThumbstickBttn = "P2 RB";
+            player_B_Bttn = "P2 B";
+        }
+        else if (player.name == "Player 3")
+        {
+            player_RightThumbStickHorizontal = "P3 Right ThumbStick Horizontal";
+            player_RightThumbStickVertical = "P3 Right ThumbStick Vertical";
+            player_LeftThumbStickHorizontal = "P3 Left ThumbStick Vertical";
+            player_LeftThumbStickVertical = "P3 Left ThumbStick Horizontal";
+            player_RightThumbstickBttn = "P3 RB";
+            player_B_Bttn = "P3 B";
+        }
+        else if (player.name == "Player 4")
+        {
+            player_RightThumbStickHorizontal = "P4 Right ThumbStick Horizontal";
+            player_RightThumbStickVertical = "P4 Right ThumbStick Vertical";
+            player_LeftThumbStickHorizontal = "P4 Left ThumbStick Vertical";
+            player_LeftThumbStickVertical = "P4 Left ThumbStick Horizontal";
+            player_RightThumbstickBttn = "P4 RB";
+            player_B_Bttn = "P4 B";
+        }
+        #endregion
 
+        #region Camera
         // Control camera movement
         if (cameraCanMove)
         {
-            if (Input.GetAxis("P1 Right ThumbStick Horizontal") != 0 || Input.GetAxis("P1 Right ThumbStick Vertical") != 0)
+            if (Input.GetAxis(player_RightThumbStickHorizontal) != 0 || Input.GetAxis(player_RightThumbStickVertical) != 0)
             {
-                yaw = transform.localEulerAngles.y + Input.GetAxis("P1 Right ThumbStick Horizontal") * mouseSensitivity;
+                yaw = transform.localEulerAngles.y + Input.GetAxis(player_RightThumbStickHorizontal) * mouseSensitivity;
 
                 if (!invertCamera)
                 {
-                    pitch -= mouseSensitivity * Input.GetAxis("P1 Right ThumbStick Vertical");
+                    pitch -= mouseSensitivity * Input.GetAxis(player_RightThumbStickVertical);
                 }
                 else
                 {
                     // Inverted Y
-                    pitch += mouseSensitivity * Input.GetAxis("P1 Right ThumbStick Vertical");
+                    pitch += mouseSensitivity * Input.GetAxis(player_RightThumbStickVertical);
                 }
 
                 // Clamp pitch between lookAngle
@@ -265,23 +313,7 @@ public class FPS_Controller : MonoBehaviour
 
         if (enableCrouch)
         {
-            //if (Input.GetKeyDown(crouchKey) && !holdToCrouch)
-            //{
-            //    Crouch();
-            //}
-
-            //if (Input.GetKeyDown(crouchKey) && holdToCrouch)
-            //{
-            //    isCrouched = false;
-            //    Crouch();
-            //}
-            //else if (Input.GetKeyUp(crouchKey) && holdToCrouch)
-            //{
-            //    isCrouched = true;
-            //    Crouch();
-            //}
-
-            if (Input.GetButtonDown("P1 B"))
+            if (Input.GetButtonDown(player_B_Bttn))
             {
                 if (!holdToCrouch)
                 {
@@ -294,7 +326,7 @@ public class FPS_Controller : MonoBehaviour
                 }
             }
 
-            if (Input.GetButtonUp("P1 B"))
+            if (Input.GetButtonUp(player_B_Bttn))
             {
                 if (holdToCrouch)
                 {
@@ -320,10 +352,10 @@ public class FPS_Controller : MonoBehaviour
 
         if (playerCanMove)
         {
-            if (Input.GetAxis("P1 Left ThumbStick Vertical") != 0 || Input.GetAxis("P1 Left ThumbStick Horizontal") != 0)
+            if (Input.GetAxis(player_LeftThumbStickVertical) != 0 || Input.GetAxis(player_LeftThumbStickHorizontal) != 0)
             {
                 // Calculate how fast we should be moving
-                targetVelocity = new Vector3(Input.GetAxis("P1 Left ThumbStick Vertical"), 0, Input.GetAxis("P1 Left ThumbStick Horizontal"));
+                targetVelocity = new Vector3(Input.GetAxis(player_LeftThumbStickHorizontal), 0, Input.GetAxis(player_LeftThumbStickVertical));
                 isWalking = true;
             }
             else
@@ -343,7 +375,7 @@ public class FPS_Controller : MonoBehaviour
             //}
 
             // All movement calculations shile sprint is active
-                if (enableSprint && Input.GetButton("P1 RB") && sprintRemaining > 0f && !isSprintCooldown)
+                if (enableSprint && Input.GetButton(player_RightThumbstickBttn) && sprintRemaining > 0f && !isSprintCooldown)
             {
                 if (currentSprintSpeed < 8)
                 {
