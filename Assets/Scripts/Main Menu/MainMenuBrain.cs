dd3f24233dc64e4beba2a9f0240b88668c85735e
Mingrui <limingrui0810@gmail.com>
2024-03-30 18:44:03 +0800

    mainmenu

--- Assets/Scripts/Main Menu/MainMenuBrain.cs
+++ Assets/Scripts/Main Menu/MainMenuBrain.cs
@@ -2,12 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 public class MainMenuBrain : MonoBehaviour
 {
+    public Button OptionBtn;
     // Start is called before the first frame update
     void Start()
     {
-        
+        OptionBtn.interactable = false;
     }
 
     // Update is called once per frame
