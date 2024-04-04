1bab9efa7e33849de531a37822f9349a986749ca
Kaizatan <kaishenkaiser.t@digipen.edu>
2024-04-04 14:55:03 +0800

    Pickup Update

--- Assets/Scenes/Character Anim Event Triggers/PlayerEventTrigger.cs
+++ Assets/Scenes/Character Anim Event Triggers/PlayerEventTrigger.cs
@@ -9,15 +9,46 @@ public class PlayerEventTrigger : MonoBehaviour
     [Header("Other Variables")]
     public FurbyPicker furbyPickerScript;
 
+    #region Player Animation Booleans
+    public void SetRun(bool option)
+    {
+        playerAnim.SetBool("Run", option);
+    }
+    public void SetCarryRun(bool option)
+    {
+        playerAnim.SetBool("CarryRun", option);
+    }
+
     public void SetPickUp(bool option)
     {
         playerAnim.SetBool("PickUp", option);
+    }
+
+    public void SetCrouch(bool option)
+    {
+        playerAnim.SetBool("Crouch", option);
     }
 
     public void SetCarry(bool option)
     {
         playerAnim.SetBool("Carry", option);
     }
+
+    public void SetCrouchWalk(bool option)
+    {
+        playerAnim.SetBool("CrouchWalk", option);
+    }
+
+    public void SetStartShaking(bool option)
+    {
+        playerAnim.SetBool("StartShaking", option);
+    }
+
+    public void SetShaking(bool option)
+    {
+        playerAnim.SetBool("Shaking", option);
+    }
+    #endregion
 
     public void ChangePickUpToCarry()
     {
